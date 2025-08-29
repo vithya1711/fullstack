ce9b0f7ad6caf34b3bb30dd0f74191e70a6c8883
vithya1711 <vithyasri.23it@npsbcet.edu.in>
2025-08-29 05:47:23 +0000

    Add files via upload

---
+++ Vehicle.java
@@ -0,0 +1,14 @@
+public class Vechile{
+public void applyBrake(){
+system.out.println("Brake Applied");
+}
+public void applyHorn(){
+System.out.println("Horn Applied");
+}
+public static void main(String[] args){
+Vechile veh=new Vechile();
+veh.applyBrake();
+veh.applyHorn();
+}
+}
+ 
